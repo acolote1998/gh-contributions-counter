611f07e4524461cb98027ddc13617bed1c694612
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-09-15 18:50:30 +0200

    refactoring printing logic to a different function

---
+++ api/util/printContributionsForUserList.ts
@@ -0,0 +1,20 @@
+import { getContributionsPerPeriodPerUser } from "./getContributionsPerPeriod";
+import { users } from "./listOfUsers";
+
+export const printContributionsForUserList = async () => {
+  // Reads command-line arguments: start date and end date (format: YYYY-MM-DD)
+  // Example usage: npm start -- 2025-09-15 2025-09-15
+
+  const args = process.argv.slice(2);
+  const fromDate = args[0];
+  const untilDate = args[1];
+  for (let i = 0; i < users.length; i++) {
+    let name: string = users[i].name;
+    const userContributions: number = await getContributionsPerPeriodPerUser(
+      users[i].ghId,
+      fromDate,
+      untilDate
+    );
+    console.log(`${name}: ${userContributions}`);
+  }
+};
--- checkContributions.ts
+++ checkContributions.ts
@@ -1,22 +1,9 @@
-import { getContributionsPerPeriodPerUser } from "./api/util/getContributionsPerPeriod";
-import { users } from "./api/util/listOfUsers";
-
-const printContributions = async () => {
-  // Reads command-line arguments: start date and end date (format: YYYY-MM-DD)
-  // Example usage: npm start -- 2025-09-15 2025-09-15
-
-  const args = process.argv.slice(2);
-  const fromDate = args[0];
-  const untilDate = args[1];
-  for (let i = 0; i < users.length; i++) {
-    let name: string = users[i].name;
-    const userContributions: number = await getContributionsPerPeriodPerUser(
-      users[i].ghId,
-      fromDate,
-      untilDate
-    );
-    console.log(`${name}: ${userContributions}`);
-  }
+import { printContributionsForUserList } from "./api/util/printContributionsForUserList";
+const run = () => {
+  console.log("##################");
+  console.log("Contributions:");
+  console.log("##################");
+  printContributionsForUserList();
 };
 
-printContributions();
+run();
