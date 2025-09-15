a8fa65a8d92528f268832b31471a42a260cb92d8
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-09-15 16:25:46 +0200

    updating config and making test fetch

--- checkContributions.ts
+++ checkContributions.ts
@@ -1,1 +1,7 @@
-console.log("test");
+import { fetchContributions } from "./api/fetchContributions";
+const test = async () => {
+  const contributions = await fetchContributions("acolote1998");
+  console.log(contributions.total[2025]);
+};
+
+test();
